87115e1aeb5e50bd0be3c69e510408d95ed3d698
Abhishek Mishra <Abhishekbelaganj0609@gmail.com>
2025-01-04 22:29:17 +0530

    fixed

--- src/Components/LBQUIZ/Navbar.js
+++ src/Components/LBQUIZ/Navbar.js
@@ -78,6 +78,7 @@ const Navbar = () => {
             <li>
               <Link
                 to="/contact"
+                onClick={closeMenu}
                 className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Contact Me
